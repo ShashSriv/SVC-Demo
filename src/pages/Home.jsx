7adc141e5ec4377e16f666960ce30c8c71c52871
zaf22 <zaf22@pitt.edu>
2024-03-18 20:35:34 -0400

    editable description and category

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import AuthDetails from '../components/auth/AuthDetails'
 import { useNavigate } from 'react-router-dom';
 import {db, auth} from '../firebase';
-import { collection, doc, deleteDoc, updateDoc, onSnapshot } from 'firebase/firestore';
+import { collection, doc, deleteDoc, updateDoc, onSnapshot, getDocs } from 'firebase/firestore';
 import { Button, Container, Table, Form } from 'react-bootstrap';
 import {onAuthStateChanged} from 'firebase/auth';
 
@@ -11,7 +11,7 @@ const Home = () => {
   const userAdd = () => {
         navigate("/addItem");
     }
-  
+  // item list
   const [itemList, setItemList] = useState([]);
   const [search, setSearch] = useState("");
 
@@ -31,10 +31,28 @@ const Home = () => {
     getList();
   }, []);
 
+  // categories list
+  const [categoryList, setCategoryList] = useState([]);
+  useEffect(() => {
+      const collectionRef1 = collection(db, "categories");
+      const getList = async () => {
+      try{
+          const data = await getDocs(collectionRef1);
+          const filteredData = data.docs.map((doc) => ({...doc.data()}));
+          setCategoryList(filteredData);     
+      }catch(error){
+          console.log(error);
+      }
+    };
+    getList();
+  }, []);
+
   // update item
   const [editItem, setEditItem] = useState(null);
   const [editedQty, setEditedQty] = useState('');
+  const [editedDescription, setEditedDescription] = useState('');
   const [editedCost, setEditedCost] = useState('');
+  const [editedCategory, setEditedCategory] = useState('');
 
   const updateItem = async (id) => {
     try{
@@ -44,14 +62,26 @@ const Home = () => {
           CostEA: editedCost
         });
       }
+      if (editedDescription !== ''){
+        await updateDoc(ref, {
+          Description: editedDescription
+        });
+      }
       if (editedQty !== ''){
         await updateDoc(ref, {
           Qty: editedQty
         });
       }
+      if (editedCategory !== ''){
+        await updateDoc(ref, {
+          Category: editedCategory
+        });
+      }
       setEditItem('');
       setEditedCost('');
+      setEditedDescription('');
       setEditedQty('');
+      setEditedCategory('');
     }catch(error){
       console.log(error);
     }
@@ -116,7 +146,11 @@ const Home = () => {
                   (item.Qty)}
                 </td>
 
-                <td>{item.Description}</td>
+                <td>{editItem === item.id ? (
+                    <Form.Control type='text' value={editedDescription === '' ? item.Description : editedDescription} onChange={(e) => setEditedDescription(e.target.value)} />
+                ) : 
+                (item.Description)}
+                </td>
 
                 <td>
                   {editItem === item.id ? (
@@ -125,13 +159,25 @@ const Home = () => {
                   (item.CostEA)}
                 </td>
 
-                <td>{item.Category}</td>
+                <td>{editItem === item.id ? (
+                    <Form.Select value={editedCategory === '' ? item.Category : editedCategory} onChange={(e) => setEditedCategory(e.target.value)}>
+                        {categoryList.map((item1) => (
+                          <option>{item1.category}</option>
+                        ))}
+                    </Form.Select>
+                  ) : (item.Category)}
+                </td>
 
                 <td>
                   {editItem === item.id ? (
                     <>
                     <Button onClick={() => {updateItem(item.id)}}>Save</Button> {' '}
-                    <Button onClick={() => {setEditItem(''); setEditedCost(''); setEditedQty('');}}>Cancel</Button>
+                    <Button onClick={() => {
+                      setEditItem(''); 
+                      setEditedCost(''); 
+                      setEditedDescription(''); 
+                      setEditedQty(''); 
+                      setEditedCategory('');}}>Cancel</Button>
                     </>
                   ) : (
                     <>
